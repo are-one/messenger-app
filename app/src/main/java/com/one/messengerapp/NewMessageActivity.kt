1a5e87bd48c21fe64888ce9b4375397dbd1d99f3
Are1PM <arwanpriantomangidi@gmail.com>
2021-06-19 19:55:33 +0800

    implement groupie library

--- app/src/main/java/com/one/messengerapp/NewMessageActivity.kt
+++ app/src/main/java/com/one/messengerapp/NewMessageActivity.kt
@@ -1,9 +1,15 @@
 package com.one.messengerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.one.messengerapp.databinding.ActivityNewMessageBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 
 class NewMessageActivity : AppCompatActivity() {
 
@@ -17,6 +23,22 @@ class NewMessageActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Select User"
 
-        binding.recyclerviewNewmessage.adapter
+        val adapter = GroupAdapter<GroupieViewHolder>()
+
+        adapter.add(UserItem())
+
+        binding.recyclerviewNewmessage.adapter = adapter
+
     }
+}
+
+class UserItem: Item<GroupieViewHolder>() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        // will be implement
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+
 }
