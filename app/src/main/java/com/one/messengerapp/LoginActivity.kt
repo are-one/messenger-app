8e3bb0b2e89d03d13605ec7b8dace9c32c2ad6cf
Are1PM <arwanpriantomangidi@gmail.com>
2021-06-23 13:20:12 +0800

    create login

--- app/src/main/java/com/one/messengerapp/LoginActivity.kt
+++ app/src/main/java/com/one/messengerapp/LoginActivity.kt
@@ -1,11 +1,46 @@
 package com.one.messengerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.one.messengerapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        supportActionBar?.title = "Login"
+
+        binding.buttonLogin.setOnClickListener {
+            loggin()
+        }
+    }
+
+    private fun loggin() {
+        val email = binding.edittextEmailLogin.text.toString()
+        val password = binding.edittextPasswordLogin.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please input email/password!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    val intent = Intent(this, LatestMessagesActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed login: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
     }
 }
